5d47886f4e4c465aefbf3da9667e8853d277d3eb
oshan <oshan@HP-I3>
2022-05-30 09:31:44 -0700

    oshan

---
+++ src/src/DB.java
@@ -0,0 +1,22 @@
+
+package src;
+
+import java.sql.*;
+public class DB {
+    
+    static Connection con = null;
+    
+    public static Connection getConnection(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/iba","iba","oshan62426974");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+    
+    
+}
